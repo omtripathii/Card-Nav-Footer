83848cacb402c34c986f0d3f1c8866105e015621
OM TRIPATHI <109409112+omtripathii@users.noreply.github.com>
2024-01-24 01:20:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import Navigation from './components/navigation/Navigation'
-import Section1 from './components/section1/Section1'
-import Section2 from './components/section2/Section2'
-import Section3 from './components/section3/Section3'
-import Section4 from './components/section4/Section4'
 import Footer from './components/footer/Footer'
 
 
@@ -14,10 +10,6 @@ function App() {
     <div className='wrapper'>
 
       <Navigation />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
       <Footer />
 
     </div>
